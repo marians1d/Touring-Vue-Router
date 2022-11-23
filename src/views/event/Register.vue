15aec330bb403697e9d21286910d875a16dd9286
Mariyan Damyanov <marians1md@gmail.com>
2022-11-23 22:07:36 +0200

    Implementing programmatic redirect

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,21 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      // Call API
+
+      setTimeout(() => {
+        this.$router.push({
+          name: 'EventDetails'
+        })
+      }, 1000)
+    }
+  }
 }
 </script>
